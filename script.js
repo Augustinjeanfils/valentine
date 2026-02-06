8a65b51ec0168400c402499789fbfd156126345a
Augustinjeanfils <166634946+Augustinjeanfils@users.noreply.github.com>
2026-02-06 11:42:54 -0500

    add photo

--- script.js
+++ script.js
@@ -2,9 +2,3 @@ function afficherSouvenir() {
     document.getElementById('souvenir').style.display = 'block';
 }
 
-
-function afficherSouvenir() {
-    console.log("Le bouton a été cliqué !"); // Ceci s'affichera dans la console (F12)
-    alert("Voici nos premier souvenir 🥺"); // Ceci fera apparaître une petite fenêtre
-    document.getElementById('souvenir').style.display = 'block';
-}
