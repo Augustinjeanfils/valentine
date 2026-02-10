f3be2ae71679064023a3682ce3a6e45662690b29
Augustinjeanfils <166634946+Augustinjeanfils@users.noreply.github.com>
2026-02-10 14:08:08 -0500

    add a message

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function afficherSouvenir() {
     document.getElementById('souvenir').style.display = 'block';
 }
 
+function afficherMessage(){
+    alert("Je t'aime Princess👸❤️")
+    document.querySelector('.container').style.display = 'block';
+}
